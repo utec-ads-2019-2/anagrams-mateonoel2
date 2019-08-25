87a75847be189eb6da71ec3bc881c1283a7a1e8a
mateonoel2 <39866695+mateonoel2@users.noreply.github.com>
2019-08-25 11:47:23 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
-}
+int main(){
+    string palabra;
+    string temporary = "0";
+    vector<string> ordered;
+    vector<string> desordered;
+    vector<string> passed;
+    vector<string> print;
+    vector<int> not_pass;
 
+    while(cin>>palabra){
+        if(palabra == "#")
+            break;
+        desordered.push_back(palabra);
+
+        for_each(palabra.begin(), palabra.end(), [](char & c) {
+            c = ::tolower(c);
+        });
+
+        sort(palabra.begin(), palabra.end());
+        ordered.push_back(palabra);
+    }
+
+    for(int i=0; i<=ordered.size(); i++){
+        temporary = ordered[i];
+        ordered[i]="0";
+            if((find(ordered.begin(), ordered.end(), temporary) != ordered.end())){
+                not_pass.push_back(i);
+            }
+        ordered[i]=temporary;
+    }
+
+    for(int i=0; i<= desordered.size(); i++) {
+        if (!(find(not_pass.begin(), not_pass.end(), i) != not_pass.end())){
+            print.push_back(desordered[i]);
+        }
+    }
+
+    sort(print.begin(), print.end());
+
+    for(int i=0; i<= print.size(); i++){
+        if(!print[i].empty()){
+            cout<<print[i]<<'\n';
+        }
+    }
+
+    return 0;
+}
